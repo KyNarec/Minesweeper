a6c4d602d7c7367272c864999fce162bd5d4be35
KyNarec <jim48@gmx.de>
2025-07-15 20:39:50 +0200

    changed resizeable

--- src/main/java/org/minesweeper/graphics/GUI.java
+++ src/main/java/org/minesweeper/graphics/GUI.java
@@ -5,12 +5,13 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 public class GUI extends JFrame {
-    public GUI() {
-        setTitle("Minesweeper");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(600, 600);
-        Board board = new Board(6, 6);
-        add(board, BorderLayout.CENTER);
-        pack();
-    }
+  public GUI() {
+    setTitle("Minesweeper");
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setSize(600, 600);
+    setResizable(false);
+    Board board = new Board(6, 6);
+    add(board, BorderLayout.CENTER);
+    pack();
+  }
 }
