411edaf54ddf4fa5cabf1e8833c35ac15013432c
Gameovernlp <nikmann.nm@gmail.com>
2025-07-22 15:20:38 +0200

    created click

--- src/main/java/org/minesweeper/logic/Logic.java
+++ src/main/java/org/minesweeper/logic/Logic.java
@@ -14,21 +14,51 @@ public class Logic {
     //amount of bombs
     private int nbomb;
 
+    //firstclick
+    private boolean firstclick;
 
-    public Logic(int size, int bombs){
+
+
+    public Logic(int size, int bombs) {
 
         //setting of variables
         open = new boolean[size][size];
         bomb = new boolean[size][size];
         nbomb = bombs;
+        firstclick = true;
 
         //setting field to false
-        for(int i = 0;i<size;i++){
-            for(int j = 0;j<size;j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 open[i][j] = false;
             }
         }
 
+    }
 
+
+
+    public void leftClick(int x , int y) {
+        if (open[x][y]) {
+        } else {
+            if (firstclick) {
+                generate();
+            } else {
+                if (bomb[x][y]) {
+                    lost();
+                } else {
+                    int attachedBombs = countBombs(x, y);
+
+
+                }
+            }
+
+
+        }
+    }
+
+
+    public countBombs(int x, int y){
+    if
     }
 }
