d0327dd08f9169f31c174225573b64f3043d7913
S211054 <jim48@gmx.de>
2025-07-25 09:26:03 +0200

    added set bombs and set size screen

--- src/main/java/org/minesweeper/graphics/MainMenu.java
+++ src/main/java/org/minesweeper/graphics/MainMenu.java
@@ -2,11 +2,24 @@ package org.minesweeper.graphics;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu extends JMenuBar {
 
-    public MainMenu(){
-        add(new JMenu("Play"));
-        add(new JMenu("Options"));
+    public MainMenu(GUI gui){
+        JMenu play = new JMenu("Play");
+        JMenu options = new JMenu("Options");
+
+        JMenuItem setSize = new JMenuItem("Set size of game");
+        setSize.addActionListener(ev -> gui.setSizeScreen());
+        options.add(setSize);
+
+        JMenuItem setBombs = new JMenuItem("Set amount of bombs");
+        setBombs.addActionListener(ev -> gui.setBombsScreen());
+        options.add(setBombs);
+
+        add(play);
+        add(options);
     }
 }
